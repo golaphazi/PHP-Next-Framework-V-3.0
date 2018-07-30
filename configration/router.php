c80ca35cc4181a03c8e3cd3b576692daad4d0ec5
Golap Hazi <golaphazi@gmail.com>
2018-07-30 16:06:28 +0600

    Update router.php

--- configration/router.php
+++ configration/router.php
@@ -36,26 +36,6 @@ $route['register'] 			= 'registration_module/registration@index';
 $route['login'] 			= 'registration_module/registration@login';
 $route['logout'] 			= 'registration_module/logout@logout_user';
 
-$route['manage-item'] 		= 'profile_module/account@manage_items';
-$route['upload-item'] 		= 'profile_module/account@upload_items';
-$route['upload-project'] 	= 'profile_module/account@upload_project';
-$route['json_search_category'] 				= 'profile_module/account@category_search';
-$route['json_search_category_filter'] 		= 'profile_module/account@category_filter_search';
-$route['json_search_sub_category_filter'] 	= 'profile_module/account@sub_category_filter_search';
-$route['json_search_category_delivery'] 	= 'profile_module/account@category_delivery_search';
-$route['json_search_category_project'] 				= 'profile_module/account@category_search_project';
-
-$route['manage-script'] 	= 'profile_module/account@manage_script';
-$route['upload-script'] 	= 'profile_module/account@upload_script';
-
-$route['account-setting'] 	= 'profile_module/account@index';
-$route['json_searchskills'] = 'profile_module/account@searchskills';
-$route['json_searchtags'] 	= 'profile_module/account@searchtags';
-
-$route['notification'] 		= 'profile_module/account@notification';
-$route['messages'] 			= 'profile_module/account@messages';
-$route['categories'] 		= 'product_module/categorie@index';
-
 
 
 
